82be5642d51271484101ab09513886e9f0432d59
Ankur Khandelwal <ankurkhandelwal30@gmail.com>
2023-05-20 14:15:36 +0000

    Minor fix

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    html_title = "<head><title>Udacity - Capstone Project</title></head>"
-    html_h1 = "<h1 style='text-align: center;'>Hello World! Welcome to Ankur's Cloud DevOps Engineer Capstone Project!</h1>"
-    return html_title,html_h1
+    html = "<head><title>Udacity - Capstone Project</title></head><h1 style='text-align: center;'>Hello World! Welcome to Ankur's Cloud DevOps Engineer Capstone Project!</h1>"
+    return html
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
