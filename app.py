ad48efc22917cf9aea0d18029406beebda23e752
Ankur Khandelwal <ankurkhandelwal30@gmail.com>
2023-05-20 14:28:27 +0000

    Syntacx fix

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   html_template = """<html>
-   <head>
-   <title>Udacity - Capstone project</title>
-   </head>
-   <body>
-   <h1 style='text-align: center;'>Hello World! Welcome to Ankur's Cloud DevOps Engineer Capstone Project!</h1>
-   </body>
-   </html>
-   """
+    html_template = """<html>
+    <head>
+    <title>Udacity - Capstone project</title>
+    </head>
+    <body>
+    <h1 style='text-align: center;'>Hello World! Welcome to Ankur's Cloud DevOps Engineer Capstone Project!</h1>
+    </body>
+    </html>
+    """
     
-   return html_template
+    return html_template
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
